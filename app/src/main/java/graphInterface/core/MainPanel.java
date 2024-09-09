58c03a935e0e80a38c9459d8ae5f8b5d0d30899a
Maddy Walkington <alecwalkington@Maddys-MacBook-Air.local>
2024-09-09 18:17:04 -0400

    Editing the panel hierarchy

--- app/src/main/java/graphInterface/core/MainPanel.java
+++ app/src/main/java/graphInterface/core/MainPanel.java
@@ -11,10 +11,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
 
-import graphInterface.simulation.FirstPanel;
 import graphInterface.simulation.SimulationP;
 
 public class MainPanel extends JPanel {
+  boolean simRunning = false;
 
   private static final long serialVersionUID = 6992977983635887677L;
 
@@ -34,21 +34,24 @@ public class MainPanel extends JPanel {
     springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 75, SpringLayout.NORTH, this);
     this.add(lblNewLabel);
 
-    FirstPanel firstPanel = new FirstPanel(new Rectangle((int) this.getBounds().getWidth()/3,(int) this.getBounds().getHeight()/3) );
-    springLayout.putConstraint(SpringLayout.SOUTH, firstPanel, 0, SpringLayout.SOUTH, this);
-    springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, firstPanel, 0, SpringLayout.HORIZONTAL_CENTER, this);
-    this.add(firstPanel);
 
-    SimulationP simPanel = new SimulationP(new Rectangle((int) this.getBounds().getWidth()/4,(int) this.getBounds().getHeight()/4));
-    springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, simPanel, 0, SpringLayout.VERTICAL_CENTER, this);
-    springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, simPanel, 0, SpringLayout.HORIZONTAL_CENTER, this);
-    this.add(simPanel);
+
 
     JPanel thisP = this;
-    JButton btnNewButton = new JButton("Get dimensions");
+    JButton btnNewButton = new JButton("Start Simulation");
     btnNewButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        System.out.println(thisP.getBounds());
+        if (!simRunning) {
+          SimulationP simPanel = new SimulationP(new Rectangle((int) thisP.getBounds().getWidth() / 4, (int) thisP.getBounds().getHeight() / 4));
+          springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, simPanel, 0, SpringLayout.VERTICAL_CENTER, thisP);
+          springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, simPanel, 0, SpringLayout.HORIZONTAL_CENTER, thisP);
+          thisP.add(simPanel);
+
+          thisP.revalidate();
+
+          simRunning = true;
+        }
+
       }
     });
     springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, btnNewButton, 0, SpringLayout.HORIZONTAL_CENTER, this);
