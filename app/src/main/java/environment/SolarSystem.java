94c7ea47b10b10e0ce0c08ac21225790c9275170
Maddy Walkington <alecwalkington@Maddys-MacBook-Air.local>
2024-09-23 17:44:01 -0400

    Make the solar system with proper distances and radii

--- app/src/main/java/environment/SolarSystem.java
+++ app/src/main/java/environment/SolarSystem.java
@@ -20,16 +20,17 @@ public class SolarSystem implements Paintable {
    * Constructor for the Solar System
    */
   public SolarSystem() {
-    bodies.add(new Star(696340000, 1000, new Vector3D(9e9, 0, 100), new Vector3D()));
-    bodies.add(new RockyPlanet(2439, 1000, new Vector3D(8.948e9, 0, 100), new Vector3D()));
-    bodies.add(new RockyPlanet(6051, 1000, new Vector3D(8.892e9, 0, 100), new Vector3D()));
-    bodies.add(new RockyPlanet(6371, 1000, new Vector3D(8.85e9, 0, 100), new Vector3D()));
-    bodies.add(new RockyPlanet(3389, 1000, new Vector3D(8.776e9, 0, 100), new Vector3D()));
-    bodies.add(new Asteroid(150000000, 1000, new Vector3D(8.671e9, 100, 0), new Vector3D()));
-    bodies.add(new GassyPlanet(69911, 1000, new Vector3D(8.245e9, 0, 100), new Vector3D()));
-    bodies.add(new GassyPlanet(60268, 1000, new Vector3D(7.5e9, 0, 100), new Vector3D()));
-    bodies.add(new GassyPlanet(25362, 1000, new Vector3D(6.1e9, 0, 100), new Vector3D()));
-    bodies.add(new GassyPlanet(24622, 1000, new Vector3D(4.6e9, 0, 100), new Vector3D()));
+    bodies.add(new Star(6.96340e8, 1000, new Vector3D(0, 0, 100), new Vector3D()));
+    bodies.add(new Star(6.96340e8, 1000, new Vector3D(2e11, 0, 100), new Vector3D()));
+    bodies.add(new RockyPlanet(2.4397e6, 1000, new Vector3D(53.686e9, 0, 100), new Vector3D()));
+    bodies.add(new RockyPlanet(6.0518e6, 1000, new Vector3D(108.59e9, 0, 100), new Vector3D()));
+    bodies.add(new RockyPlanet(6.371e6, 1000, new Vector3D(150.08e9, 0, 100), new Vector3D()));
+    bodies.add(new RockyPlanet(3.3895e6, 1000, new Vector3D(224.75e9, 0, 100), new Vector3D()));
+    //bodies.add(new Asteroid(150e9, 1000, new Vector3D(405e9, 100, 0), new Vector3D()));
+    bodies.add(new GassyPlanet(69.911e6, 1000, new Vector3D(755.31e9, 0, 100), new Vector3D()));
+    bodies.add(new GassyPlanet(58.232e6, 1000, new Vector3D(1.4442e12, 0, 100), new Vector3D()));
+    bodies.add(new GassyPlanet(25.362e6, 1000, new Vector3D(2.9264e12, 0, 100), new Vector3D()));
+    bodies.add(new GassyPlanet(24.622e6, 1000, new Vector3D(4.4717e12, 0, 100), new Vector3D()));
   }
 
   public void paintThis(Graphics2D g2d) {
